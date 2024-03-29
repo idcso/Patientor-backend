5718316cf836964fbffec41255c78bcd312a8405
idcso <52316112+idcso@users.noreply.github.com>
2024-03-29 22:37:33 +0500

    Change PORT value and configure CORS

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-const PORT = 3003;
+const PORT = 3001;
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/ping', (_req, res) => {
