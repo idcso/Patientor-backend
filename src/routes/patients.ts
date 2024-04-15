a37217033913e388086cbc2610c278510624c1f1
idcso <52316112+idcso@users.noreply.github.com>
2024-04-15 23:12:56 +0500

    Implement endpoint to show single patient's info

--- src/routes/patients.ts
+++ src/routes/patients.ts
@@ -23,4 +23,9 @@ router.post('/', (req, res) => {
   }
 });
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  res.json(patientService.getPatientById(id));
+});
+
 export default router;
--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -1,12 +1,12 @@
 import patients from '../../data/patients';
-import { PatientEntry, NoSsnPatientEntry, NewPatientEntry } from '../types';
+import { PatientEntry, NonSensitivePatient, NewPatientEntry } from '../types';
 import { v1 } from 'uuid';
 
 const getEntries = (): PatientEntry[] => {
   return patients;
 };
 
-const getNoSsnEntries = (): NoSsnPatientEntry[] => {
+const getNoSsnEntries = (): NonSensitivePatient[] => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => {
     return {
       id,
@@ -28,8 +28,15 @@ const addNew = (entry: NewPatientEntry): PatientEntry => {
   return newPatientEntry;
 };
 
+const getPatientById = (id: string): PatientEntry | undefined => {
+  const patient = patients.find((patient) => patient.id === id);
+  if (!patient) return;
+  return patient;
+};
+
 export default {
   getEntries,
   getNoSsnEntries,
   addNew,
+  getPatientById,
 };
